397bf93af730ae8584421a474b55e6411df30ddf
Kağan Uğur <kgnugur@gmail.com>
2024-05-19 16:43:38 +0300

    Add autocommand for prettierd

--- generic-neovim/.config/nvim/lua/config/autocmds.lua
+++ generic-neovim/.config/nvim/lua/config/autocmds.lua
@@ -19,3 +19,12 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.wrap = true
 	end,
 })
+
+-- https://github.com/fsouza/prettierd/issues/719#issuecomment-2078544807
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	group = vim.api.nvim_create_augroup("RestartPrettierd", { clear = true }),
+	pattern = "*prettier*",
+	callback = function()
+		vim.fn.system("prettierd restart")
+	end,
+})
