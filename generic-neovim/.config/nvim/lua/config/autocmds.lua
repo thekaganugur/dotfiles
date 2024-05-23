dd2943d40f565a8175167622bd3240c031fb895c
Kağan Uğur <kgnugur@gmail.com>
2024-05-23 13:48:20 +0300

    Refactor to use vim options with lua better

--- generic-neovim/.config/nvim/lua/config/autocmds.lua
+++ generic-neovim/.config/nvim/lua/config/autocmds.lua
@@ -1,7 +1,7 @@
 -- Fixes Autocomment
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
 	callback = function()
-		vim.cmd("set formatoptions-=cro")
+		vim.opt.formatoptions:remove({ "c", "r", "o" })
 	end,
 })
 
